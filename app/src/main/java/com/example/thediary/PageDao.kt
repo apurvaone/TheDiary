acf35121898827f9ee9b68f2707c997228d1a96d
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-07-14 18:37:29 +0530

    DAO interface added

---
+++ app/src/main/java/com/example/thediary/PageDao.kt
@@ -0,0 +1,20 @@
+package com.example.thediary
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PageDao
+{
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(page: Page)
+
+    @Query("DELETE FROM page_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM page_table ")
+    fun getAlphabetizedWords(): Flow<List<Page>>
+}
