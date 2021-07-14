95d44032afd15312ed4851434e220af9232e46e2
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-07-14 19:09:56 +0530

    Room Database class added

---
+++ app/src/main/java/com/example/thediary/PageRoomDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.thediary
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Page::class), version = 1, exportSchema = false)
+
+public abstract class PageRoomDatabase:RoomDatabase()
+{
+    abstract fun wordDao(): PageDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: PageRoomDatabase? = null
+
+        fun getDatabase(context: Context): PageRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PageRoomDatabase::class.java,
+                    "page_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
