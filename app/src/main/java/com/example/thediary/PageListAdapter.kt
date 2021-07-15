67bb094e96291490ec8fdecef15b9576238b5546
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-07-16 02:42:36 +0530

    PageListAdapter.kt added

---
+++ app/src/main/java/com/example/thediary/PageListAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.thediary
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+
+class PageListAdapter
