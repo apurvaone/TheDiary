2ed33418f3ae5eceb33a3f8e8ada5f7b6b5fed3e
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-07-16 23:04:27 +0530

    database populated

--- app/src/main/java/com/example/thediary/PageRoomDatabase.kt
+++ app/src/main/java/com/example/thediary/PageRoomDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(Page::class), version = 1, exportSchema = false)
 
@@ -11,13 +14,41 @@ public abstract class PageRoomDatabase:RoomDatabase()
 {
     abstract fun pageDao(): PageDao
 
+    private class PageDatabaseCallback(
+            private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.pageDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(pageDao: PageDao) {
+            // Delete all content here.
+            pageDao.deleteAll()
+
+            // Add sample words.
+            var page = Page("My first diary page"," this was amazing day i started python today as i plan to shift to python in full pace, i took a small pythin course from mosh and loving it also i am enjoyig android development")
+            pageDao.insert(page)
+            page = Page("Enjoyig the diary writing", "I have fallen in love with diary writing it gives great feeling of satisfaction and containment also it makes sure that i remain in good frame of mind")
+            pageDao.insert(page)
+
+            // TODO: Add your own words!
+        }
+    }
+
+
     companion object {
         // Singleton prevents multiple instances of database opening at the
         // same time.
         @Volatile
         private var INSTANCE: PageRoomDatabase? = null
 
-        fun getDatabase(context: Context): PageRoomDatabase {
+        fun getDatabase(context: Context, scope:CoroutineScope): PageRoomDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
--- app/src/main/java/com/example/thediary/PagesApplication.kt
+++ app/src/main/java/com/example/thediary/PagesApplication.kt
@@ -1,9 +1,12 @@
 package com.example.thediary
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class PagesApplication: Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
 
-    val database by lazy { PageRoomDatabase.getDatabase(this) }
+    val database by lazy { PageRoomDatabase.getDatabase(this,applicationScope) }
     val repository by lazy { PageRepository(database.pageDao()) }
 }
