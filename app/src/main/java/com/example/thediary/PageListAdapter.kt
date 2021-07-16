cb2c7a03b580e46bf3d2b07fd76df7f5bd9250ee
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-07-16 20:09:51 +0530

    PageListAdapter.kt implemented

--- app/src/main/java/com/example/thediary/PageListAdapter.kt
+++ app/src/main/java/com/example/thediary/PageListAdapter.kt
@@ -3,9 +3,62 @@ package com.example.thediary
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
+
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class PageListAdapter
+class PageListAdapter: ListAdapter<Page, PageListAdapter.PageViewHolder>(PageComparator())
+{
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PageViewHolder {
+        return PageViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: PageViewHolder, position: Int) {
+        val current= getItem(position)
+        holder.bind(current.title,current.content)
+    }
+
+
+    class PageViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+
+        private val titleItemView: TextView= itemView.findViewById(R.id.title)
+        private val contentItemView:TextView= itemView.findViewById(R.id.content)
+
+        fun bind(text1:String?,text2:String)
+        {
+            titleItemView.text= text1
+            contentItemView.text= text2
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): PageViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.recyclerview_item, parent, false)
+                return PageViewHolder(view)
+            }
+
+
+    }
+
+
+}
+    class PageComparator:DiffUtil.ItemCallback<Page>(){
+        override fun areItemsTheSame(oldItem: Page, newItem: Page): Boolean {
+            return  oldItem==newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Page, newItem: Page): Boolean {
+            return  oldItem.title==oldItem.title
+        }
+
+
+    }
+
+
+
+
+}
