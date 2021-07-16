50c0ffef4bfb0b9bbe80e6f9fce8523c639d0c15
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-07-17 00:05:42 +0530

    Intent passed from NewWordActivity

--- app/src/main/java/com/example/thediary/NewPageActivity.kt
+++ app/src/main/java/com/example/thediary/NewPageActivity.kt
@@ -1,11 +1,45 @@
 package com.example.thediary
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
 
 class NewPageActivity : AppCompatActivity() {
+
+    private lateinit var editTitle: EditText
+    private lateinit var editContent: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_page)
+
+        editTitle= findViewById(R.id.edit_title)
+        editContent= findViewById(R.id.edit_content)
+
+        val button= findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+
+            val replyIntent= Intent()
+            val extras: Bundle= Bundle()
+            extras.putString("title",editTitle.toString())
+            extras.putString("content",editContent.toString())
+
+            if (TextUtils.isEmpty(editTitle.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+        }else {
+
+                replyIntent.putExtra(EXTRA_REPLY, extras)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()   }
     }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.pagelistsql.REPLY"
+    }
+
 }
