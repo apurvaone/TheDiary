e523670b7a377bf5455f31e6e9103526c6955d77
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-08-09 22:10:11 +0530

    DatePicker added + Some improvements in NewPageActivity.kt

--- app/src/main/java/com/example/thediary/NewPageActivity.kt
+++ app/src/main/java/com/example/thediary/NewPageActivity.kt
@@ -1,24 +1,58 @@
 package com.example.thediary
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewPageActivity : AppCompatActivity() {
 
     private lateinit var editTitle: EditText
     private lateinit var editContent: EditText
+    private lateinit var dateText:TextView
+    val c = Calendar.getInstance()
+    val year = c.get(Calendar.YEAR)
+    val month = c.get(Calendar.MONTH)
+    val day = c.get(Calendar.DAY_OF_MONTH)
 
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_page)
 
         editTitle= findViewById(R.id.edit_title)
         editContent= findViewById(R.id.edit_content)
+        dateText= findViewById(R.id.dateText)
+
+        dateText.text= SimpleDateFormat("dd/MM/yyyy").format(System.currentTimeMillis())
+
+        var cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            dateText.text = sdf.format(cal.time)
+
+        }
+
+        dateText.setOnClickListener {
+            DatePickerDialog(this, dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
 
         val button = findViewById<Button>(R.id.button_save)
         button.setOnClickListener {
